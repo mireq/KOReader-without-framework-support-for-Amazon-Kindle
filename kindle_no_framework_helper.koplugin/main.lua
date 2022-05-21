039d0069cd343d38fe64cec3709a2dcb50ec37c8
Miroslav Bendík <miroslav.bendik@gmail.com>
2022-05-21 09:17:34 +0200

    Start initialization once, moved exit code to onExit

--- kindle_no_framework_helper.koplugin/main.lua
+++ kindle_no_framework_helper.koplugin/main.lua
@@ -1,6 +1,8 @@
 local Dispatcher = require("dispatcher")  -- luacheck:ignore
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 
+local initialized = false
+
 
 local Helper = WidgetContainer:new{
     name = "kindle_no_framework_helper",
@@ -8,21 +10,25 @@ local Helper = WidgetContainer:new{
 }
 
 function Helper:init()
-    local haslipc, lipc = pcall(require, "liblipclua")
-    if haslipc and lipc then
-        self.lipc_handle, error_message, error_number = lipc.init("com.lab126.kaf")
-    end
+    if not initialized then
+        local haslipc, lipc = pcall(require, "liblipclua")
+        if haslipc and lipc then
+            self.lipc_handle, error_message, error_number = lipc.init("com.lab126.kaf")
+        end
 
-    if self.lipc_handle ~= nil then
-        local framework_started_property = self.lipc_handle:register_int_property("frameworkStarted", "r")
-        framework_started_property.value = 1
-        self.lipc_handle:set_string_property("com.lab126.blanket", "unload", "splash")
-        self.lipc_handle:set_string_property("com.lab126.blanket", "unload", "screensaver")
+        if self.lipc_handle ~= nil then
+            local framework_started_property = self.lipc_handle:register_int_property("frameworkStarted", "r")
+            framework_started_property.value = 1
+            self.lipc_handle:set_string_property("com.lab126.blanket", "unload", "splash")
+            self.lipc_handle:set_string_property("com.lab126.blanket", "unload", "screensaver")
+        end
+
+        initialized = true
     end
 end
 
 
-function Helper:onCloseWidget()
+function Helper:onExit()
     if self.lipc_handle ~= nil then
         self.lipc_handle:set_string_property("com.lab126.blanket", "load", "screensaver")
         self.lipc_handle:set_string_property("com.lab126.blanket", "load", "splash")
